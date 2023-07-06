b545565de0a1fa0bbe1ecb363d3fdd2f1dee1632
Marina <m1ivanushkina@gmail.com>
2023-07-06 15:46:26 +0300

    changed price* currency.newRate in ProductListItem

--- src/Components/Products/ProductListItem.tsx
+++ src/Components/Products/ProductListItem.tsx
@@ -32,7 +32,7 @@ const ProductListItem = ({
                     <h4 className="product-title">{title}</h4>
                     <div className="product-description">{description}</div>
                     <div className="product-price">
-                        {price * currency.oldRate}
+                        {price * currency.newRate}
                     </div>
                     <CardActions className="btn-wrap">
                         <Button
